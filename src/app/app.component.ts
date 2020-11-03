84896be0dca7204eefe077c6657ea940c74cd5a9
Rafal <rk.rzeszutek@gmail.com>
2020-11-04 00:26:53 +0100

    forms fix + prescriptions view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Client';
+  router: string;
+
+  // tslint:disable-next-line:variable-name
+  constructor(public _router: Router) {
+    this.router = _router.url;
+  }
 }
